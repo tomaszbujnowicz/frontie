caefc96ea8ae9076155cff8118c44913e3296407
Tomasz Bujnowicz <tomasz@prollective.com>
2017-11-01 22:25:08 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Frontend Development Starter Kit (NodeJS/NPM/Gulp/Browsersync/Sass/HTML5 Boilerplate/Bootstrap 4 Grid system)
+
+Frontend Development Starter Kit is a lightweight front-end development starter kit using Gulp for task automation. Based on HTML5 Boilerplate and Bootstrap 4 Grid system enhanced with SASS and Browsersync.
+
+## Why another frontend development starter kit? Just to keep things simple.
+
+I like to keep things as simple as possible so when I work on smaller static pojects, I very often start from scratch just to have the better control over the code.
+
+It's the same routine and tools:
+* [Gulp](http://gulpjs.com/) for task automation
+* [HTML5 Boilerplate](https://html5boilerplate.com/)
+* [Bootstrap 4 Grid](http://getbootstrap.com/docs/4.0/layout/grid/)
+* [Bootstrap 4 Responsive Breakpoints](http://getbootstrap.com/docs/4.0/layout/overview/#responsive-breakpoints/)
+* [Sass](http://sass-lang.com/) as a CSS preprocessor
+* [Autoprefixer](https://www.npmjs.org/package/gulp-autoprefixer)
+* [Browsersync](https://www.browsersync.io/)
+
+I also use [Twig.js](https://github.com/twigjs/twig.js) as a template engine so I might include this one here as well.
+
+### Why don't you include everything from Bootstrap 4?
+
+It's a really great frontend framework for many projects but in some cases it might be just too large and complex. I only took what I needed from it which is grid system and responsive breakpoints. If you need more stuff then please feel free to add more things from this amazing framework.
+
+### Other tools
+
+There are many more nice tools out there like ES6/Bower/Babel/Babelify/Browserify which I don't need at the moment for projects based on this specific starter kit.
+
+
+## Quick start
+**Download all the project dependencies:**
+```sh
+npm install
+```
+
+**Watch and develop:**
+```sh
+gulp
+```
+
+**Generate a fresh build of your project to /dist folder:**
+```sh
+gulp build
+```
+
+## Installation requirements
+
+Not familiar with Gulp? [Getting started with gulp](http://markgoodyear.com/2014/01/getting-started-with-gulp/)
+
+## Copyright and license
+
+Copyright 2017 Tomasz Bujnowicz under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ gulpfile.js
@@ -0,0 +1,118 @@
+// Global requires
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    clean = require('gulp-clean'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    plumber = require('gulp-plumber'),
+    runSequence = require('run-sequence'),
+    del = require ('del');
+
+// Paths
+var paths = {
+  dist: 'dist/',
+  src: 'src/'
+};
+
+// Catch stream errors
+var onError = function (err) {
+  notify.onError({
+    title: "Gulp error in " + err.plugin,
+    message: err.toString()
+  })(err);
+};
+
+// Browser Sync
+gulp.task('browser-sync', function() {
+  browserSync.init(null, {
+    server: {
+      baseDir: './dist'
+    }
+  });
+});
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+// Clean dist
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+// CSS
+gulp.task('css', function () {
+  return gulp.src(paths.src + 'sass/main.scss')
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(clean())
+    .pipe(gulp.dest(paths.dist + 'css'));
+});
+gulp.task('css-watch',['css'],browserSync.reload)
+
+// JS Vendor
+gulp.task('js-vendor', function() {
+  return gulp.src(paths.src + 'js/vendor/*.js')
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(concat('vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist + 'js'));
+});
+gulp.task('js-vendor-watch',['js-vendor'],browserSync.reload)
+
+// JS Main
+gulp.task('js-main',function(){
+  return gulp.src(paths.src + 'js/main.js')
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist + 'js'));
+});
+gulp.task('js-main-watch',['js-main'],browserSync.reload)
+
+// HTML
+gulp.task('html', function() {
+  return gulp.src(paths.src + '*.html')
+    .pipe(gulp.dest(paths.dist))
+});
+gulp.task('html-watch',['html'],browserSync.reload)
+
+// Images
+gulp.task('images', function() {
+  return gulp.src([
+    paths.src + 'img/**/*'
+  ], {
+    'dot': true // include hidden files
+  })
+    .pipe(gulp.dest(paths.dist + 'img'));
+});
+gulp.task('images-watch',['images'],browserSync.reload)
+
+// Watch
+gulp.task('watch', function() {
+  gulp.watch(paths.src + '*.html', ['html-watch']);
+  gulp.watch(paths.src + 'img/**/*', ['images-watch']);
+  gulp.watch(paths.src + 'sass/**/*.scss', ['css-watch']);
+  gulp.watch(paths.src + 'js/main.js', ['js-main-watch']);
+  gulp.watch(paths.src + 'js/vendor/**/*.js', ['js-vendor-watch']);
+});
+
+// Build
+gulp.task('build', function (done) {
+  runSequence('clean:dist',
+    ['css', 'js-vendor', 'js-main', 'images', 'html'],
+    done
+  )
+})
+
+// Default
+gulp.task('default', function(done) {
+  runSequence('build',
+    ['watch', 'browser-sync'],
+    done
+  )
+});
